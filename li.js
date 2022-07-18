74e6e4c9a75b2349d308f056147034663405f3bf
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-07-18 10:27:31 +0300

    Отображение элемента после загрузки

--- li.js
+++ li.js
@@ -1,18 +1,28 @@
 // WARNING: For POST requests, body is set to null by browsers.
-let data = "apiKey=FxInv50WlpCdL5hTQUrYB3C6hVRgABrFMyEhqcm0qacXUx3K8oDgMtiH85FO";
-let balCD
-let xhr = new XMLHttpRequest();
-xhr.withCredentials = false;
+window.onload = init;
+function init() {
+    // выходим, если функция уже выполнялась
+    if (arguments.callee.done) return;
+    
+    // устанавливаем флаг, чтобы функция не исполнялась дважды
+    arguments.callee.done = true;
+    
+    let data = "apiKey=FxInv50WlpCdL5hTQUrYB3C6hVRgABrFMyEhqcm0qacXUx3K8oDgMtiH85FO";
+    let balCD
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    balCD = JSON.parse(this.responseText).data.balance;
-    $('.calldog').html(`${balCD}`)
-  }
-});
+    xhr.open("POST", "https://lk.calldog.ru/apiCalls/userInfo");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-xhr.open("POST", "https://lk.calldog.ru/apiCalls/userInfo");
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            balCD = JSON.parse(this.responseText).data.balance;
+            $('.calldog').html(`${balCD}`)
+        }
+        });
+};
+    
 
-xhr.send(data);
 
