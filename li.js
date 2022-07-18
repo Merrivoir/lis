8e16b3adb88ffa36e6191f3dc451a7d78620f643
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2022-07-18 09:35:17 +0300

    Calldog balance

--- li.js
+++ li.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-    alert("LOADED")
-})
+// WARNING: For POST requests, body is set to null by browsers.
+let data = "apiKey=FxInv50WlpCdL5hTQUrYB3C6hVRgABrFMyEhqcm0qacXUx3K8oDgMtiH85FO";
+let balCD
+let xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    balCD = JSON.parse(this.responseText).data.balance;
+    $('.hello').html(`${balCD}`)
+  }
+});
+
+xhr.open("POST", "https://lk.calldog.ru/apiCalls/userInfo");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.send(data);
+
